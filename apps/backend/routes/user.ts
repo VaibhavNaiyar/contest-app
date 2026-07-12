8ff7606c366ecff6fe8fe1bfd3a1ab730bba22f6
VaibhavNaiyar <vaibhavnaiyar04@gmail.com>
2026-07-12 10:47:49 +0530

    schema implemented

---
+++ apps/backend/routes/user.ts
@@ -0,0 +1,14 @@
+import {Router} from "express";
+
+const router = Router();
+
+router.post("/signup",(req,res) => {
+
+})
+
+
+router.post("/signin",(req,res) => {
+    
+})
+
+export default router;
